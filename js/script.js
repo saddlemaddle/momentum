8e98ef29239f41b4c947f58eab839c32d431e54c
saddlemaddle <saddlemaddle@mail.ru>
2023-02-04 16:46:34 +0600

    feat: upload layout

---
+++ js/script.js
@@ -0,0 +1,9 @@
+const time = document.querySelector('.time');
+
+const showTime = () => {
+    const date = new Date();
+    const currentTime = date.toLocaleTimeString();
+    time.textContent = currentTime;
+    setTimeout(showTime, 1000);
+}
+showTime()
