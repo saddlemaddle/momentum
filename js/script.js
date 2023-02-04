29e54d1da281d9474d2b5ff68a7d5ab874f1dd3b
saddlemaddle <saddlemaddle@mail.ru>
2023-02-04 17:24:36 +0600

    feat: add fn time

--- js/script.js
+++ js/script.js
@@ -1,9 +1,17 @@
 const time = document.querySelector('.time');
+const data = document.querySelector('.date');
 
-const showTime = () => {
+const showTime = (showDate) => {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
     time.textContent = currentTime;
     setTimeout(showTime, 1000);
 }
-showTime()
+const showDate = () => {
+    const date = new Date();
+    const options = {weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC'};
+    const currentDate = date.toLocaleDateString('ru-Ru', options);
+    data.textContent = currentDate;
+}
+showTime(showDate())
+
