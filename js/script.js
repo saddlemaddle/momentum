dcd51370468d930e24b182515d890b46f297e3ec
saddlemaddle <saddlemaddle@mail.ru>
2023-02-07 18:48:02 +0600

    feat: add random bg

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const data = document.querySelector('.date');
 const greeting = document.querySelector('.greeting')
 const name = document.querySelector('.name');
 
+
 const showTime = () => {
     const date = new Date();
     const currentTime = date.toLocaleTimeString();
@@ -11,42 +12,59 @@ const showTime = () => {
     showGreeting();
     setTimeout(showTime, 1000);
 }
+
 const showDate = () => {
     const date = new Date();
     const options = {weekday: 'long', month: 'long', day: 'numeric', timeZone: 'UTC'};
-    const currentDate = date.toLocaleDateString('ru-Ru', options);
+    const currentDate = date.toLocaleDateString('en-EN', options);
     data.textContent = currentDate;
 }
+
 const getTimeOfDay = () => {
     const date = new Date();
     const hours = date.getHours();
     console.log(hours)
     if (hours >= 6 && hours < 12) {
-        return 'Доброе утро, ';
+        return 'morning';
       } else if (hours >= 12 && hours < 18) {
-        return 'Добрый день, ';
+        return 'afternoon';
       } else if (hours >= 18 && hours < 24) {
-        return 'Добрый вечер, ';
+        return 'evening';
       } else if (hours >= 0 && hours < 6) {
-        return 'Доброй ночи, ';
+        return 'night';
       } else {
         return 'Привет, ';
       }
 }
+
 const showGreeting = () => {
     const timeOfDay = getTimeOfDay();
-    const greetingText = `${timeOfDay}`;
+    const greetingText = `Good ${timeOfDay}, `;
     greeting.textContent = greetingText;
 }
+
 function setLocalStorage() {
     localStorage.setItem('name', name.value);
   }
-  window.addEventListener('beforeunload', setLocalStorage)
+window.addEventListener('beforeunload', setLocalStorage)
 
-  function getLocalStorage() {
-    if(localStorage.getItem('name')) {
-      name.value = localStorage.getItem('name');
-    }
+function getLocalStorage() {
+  if(localStorage.getItem('name')) {
+    name.value = localStorage.getItem('name');
   }
-  window.addEventListener('load', getLocalStorage)
+}
+window.addEventListener('load', getLocalStorage)
+
+const getRandomNum = () => {
+  let randNum = String(Math.floor(Math.random() * 20) + 1);
+  return randNum;
+}
+
+const setBg = () => {
+  let timeOfDay = getTimeOfDay();
+  let bgNumb = getRandomNum();
+  bgNumb = bgNumb.padStart(2, '0');
+  return document.body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNumb}.jpg')`;
+}
+setBg();
 showTime();
